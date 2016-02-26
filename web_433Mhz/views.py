25133d90fe267dba522c9b87eb0bd614ae8556dd
tuxxy <me@johnpacific.com>
2016-02-25 21:18:29 -0700

    Add api call to open binary and grab stdout

--- web_433Mhz/views.py
+++ web_433Mhz/views.py
@@ -1,6 +1,17 @@
 from web_433Mhz import app
-from flask import render_template
+from flask import render_template, jsonify
+
+import subprocess
+import os
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@app.route('/api/get_code', methods=['GET'])
+def get_code():
+    proc = subprocess.Popen(os.path.abspath('../433Mhz'),\
+            stdout=subprocess.PIPE)
+    code = proc.communicate()[0].decode('utf-8')    # Grab the stdout
+
+    return jsonify({'code': code})
