ee490eaf0f8522657da7490632174f3add4eabf1
tuxxy <me@johnpacific.com>
2016-02-25 19:51:11 -0700

    Add package init file

---
+++ 433Mhz/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+import config
+
+app = Flask(__name__)
+app.config.from_object('config.Config')
+Bootstrap(app)
+
+from 433Mhz import views
